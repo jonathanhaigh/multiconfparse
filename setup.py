2c75b62c660dffc21204d37b4fdf9772f7a76a3a
Jonathan Haigh <jonathanhaigh@gmail.com>
2020-05-29 19:31:34 +0100

    setup.py: add package metadata
    Add a bunch of package metadata, including a version of 0.0.1.

--- setup.py
+++ setup.py
@@ -5,6 +5,32 @@
 
 import setuptools
 
+with open("README.md") as f:
+    long_desc = f.read()
+
 setuptools.setup(
-    name="multiconfig", packages=setuptools.find_packages(),
+    name="multiconfig",
+    packages=setuptools.find_packages(),
+    version="0.0.1",
+    description="Parser for configuration from multiple sources",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonathanhaigh/multiconfig",
+    author="Jonathan Haigh",
+    author_email="jonathanhaigh@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="config configuration",
+    python_requires="~=3.6",
 )
